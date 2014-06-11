6ad7918df153d78ebb09c8c50576fef2570938c2
Alex Bird <alexb@spindance.com>
2014-06-11 18:19:05 -0400

    add git aliases to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,10 @@ end
 # autojump
 if test -f ~/.autojump/etc/profile.d/autojump.fish; . ~/.autojump/etc/profile.d/autojump.fish; end
 
+# aliases
+alias gst="git status"
+alias glg="git lg"
+
 set PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
 set PATH "/usr/local/opt/gnu-tar/libexec/gnubin" $PATH
 set PATH "/usr/local/opt/gnu-sed/libexec/gnubin" $PATH
