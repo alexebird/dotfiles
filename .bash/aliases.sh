a9d25dfa080fa5e8ef7f6767d967f6d1e5b031be
Alex Bird <bird@grandroundshealth.com>
2014-11-10 15:04:50 -0800

    update git aliases

---
+++ .bash/aliases.sh
@@ -0,0 +1,9 @@
+alias genpass='< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16; echo'
+
+# git
+alias gb='git branch'
+alias gci='git commit'
+alias gco='git checkout'
+alias gd='git diff'
+alias gl='git lg'
+alias gs='git status'
