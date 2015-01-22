d570ec1a4479abb17a13427e3e4412e0c7d05946
Alex Bird <bird@grandroundshealth.com>
2015-01-21 18:27:21 -0800

    git aliases

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -9,6 +9,8 @@ alias gdc='git diff --cached'
 alias gl='git lg'
 alias gla='git lg --branches --remotes --tags'
 alias gs='git status'
+alias gp='git push'
+alias gpl='git pull'
 
 # ag
 alias ag='ag --hidden --smart-case'
