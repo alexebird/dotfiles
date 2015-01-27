2be65bd8703c015e0ffe1c3d124ae954e289e514
Alex Bird <bird@grandroundshealth.com>
2015-01-27 14:38:46 -0800

    add agignore'

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -42,5 +42,6 @@ function fuck() {
     build=$1
     min_time='4 hours ago'
     error_filter="('rspec ./') OR ('Failure/Error') OR ('# /' OR '# .')"
+    rvm use 2.1.5 > /dev/null
     papertrail --min-time "$min_time" "$build AND ($error_filter)" | less -R -X +G
 }
