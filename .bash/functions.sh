594d8bbd152a215a7c7b340da9e9df82f9ff3cb3
Alex Bird <bird@grandroundshealth.com>
2015-02-03 19:39:01 -0800

    add openbox rc.xml

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -3,18 +3,24 @@ function branch_search() {
     term=$1
 
     if [[ $term ]]; then
-        branches=$(git br | grep -v "*" | grep --color=never --ignore-case "$term" | tr -d ' ')
+        #branches=$(git br | grep -v "*" | grep --color=never --ignore-case "$term" | tr -d ' ')
+        branches=$(git for-each-ref --sort=-committerdate refs/heads/ | awk '{ print $3 }' | sed 's|refs/heads/||')
+        branch=$(echo -e "$branches" | grep --color=never --ignore-case "$term" | head -1)
 
-        if [[ "1" != $(echo -e "$branches" | wc -l) ]]; then
-            echo multiple matches: >&2
-            echo -e "$branches"    >&2
+        #if [[ "1" != $(echo -e "$branches" | wc -l) ]]; then
+            #echo -e "$branches" >&2
+            #return 1
+        #fi
+
+        if [[ -z "$branch" ]]; then
             return 1
+        else
+            echo $branch
+            return 0
         fi
-
-        echo $branches
-        return 0
     else
-        return 1
+        git rev-parse --abbrev-ref HEAD
+        return 0
     fi
 }
 
@@ -23,6 +29,11 @@ alias bs='branch_search'
 # fast git checkout
 function fco() {
     term=$1
+
+    if [ "$term" ==  "-" ]; then
+        git checkout -
+        return 0
+    fi
 
     if [ -z "$term" ]; then
         echo usage: $0 SEARCH_TERM
