3753fa9ab0f01f43313c6924c88e4dfa4e0390e4
Alex Bird <bird@grandrounds.com>
2016-02-02 13:08:25 -0800

    use xscreensaver in lubuntu config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,8 @@
-{:user  {:plugins [
-                   [venantius/ultra "0.4.0"]
-                   [cider/cider-nrepl "0.7.0"]
+{
+ :user  {:plugins [
+                   ;[venantius/ultra "0.4.0"]
+                   [lein-pprint "1.1.1"]
+                   ;[cider/cider-nrepl "0.7.0"]
                    ]}
- :ultra {:color-scheme :solarized_dark}}
+ ;:ultra {:color-scheme :solarized_dark}
+ }
