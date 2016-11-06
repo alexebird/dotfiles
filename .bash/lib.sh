b52982ec154eec751662a1c4291b6703fcc729ca
Alex Bird <alexebird@gmail.com>
2016-11-05 18:17:33 -0700

    updates to lib.sh for new linux

--- .bash/lib.sh
+++ .bash/lib.sh
@@ -1,10 +1,10 @@
 source ~/.bash/aws-user.sh
 
 alias genpass='< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c16; echo'
-alias lock='lxlock'
+#alias lock='lxlock'
 alias copy='xsel -ib'
 alias paste='xsel -b'
-alias hell='honesty-environment local'
+#alias hell='honesty-environment local'
 alias dp='docker ps --format "table {{.ID}}\t{{.Status}}\t{{.Names}}"'
 alias dpp='docker ps --format "table {{.ID}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"'
 alias wdp="watch -n1 'docker ps --format \"table {{.ID}}\t{{.Status}}\t{{.Names}}\"'"
@@ -16,8 +16,8 @@ hs() {
 }
 
 # git
-alias git='hub'
-eval "$(hub alias -s)"
+#alias git='hub'
+#eval "$(hub alias -s)"
 alias gb='git branch'
 alias gci='git commit'
 alias gco='git checkout'
@@ -74,43 +74,43 @@ alias urldecode='python -c "import urllib, sys; print urllib.unquote(sys.argv[1]
 
 # fun
 alias upgrade='sudo apt-get -y update && sudo apt-get -y dist-upgrade'
-alias shit='ssh it'
-alias rbj='ruby -rawesome_print -rjson -e'
-alias ddd='gpg -d'
-alias rr='reset'
-alias pssh="ps -eo pid,args | grep ssh | grep -v 'grep\|sshd\|ssh-agent'"
-alias myip="ifconfig eth2 | grep -o --color=none '10\.\0\.20\.[[:digit:]]\+'"
+#alias shit='ssh it'
+#alias rbj='ruby -rawesome_print -rjson -e'
+#alias ddd='gpg -d'
+#alias rr='reset'
+#alias pssh="ps -eo pid,args | grep ssh | grep -v 'grep\|sshd\|ssh-agent'"
+#alias myip="ifconfig eth2 | grep -o --color=none '10\.\0\.20\.[[:digit:]]\+'"
 alias banner='figlet -f graffiti'
-alias vpn='sudo ipsec down grnds-sfo && sudo ipsec up grnds-sfo'
-alias rz='restart zulip || start zulip'
+#alias vpn='sudo ipsec down grnds-sfo && sudo ipsec up grnds-sfo'
+#alias rz='restart zulip || start zulip'
 #alias enable-pointer='xinput set-prop 13 "Device Enabled" 1'
 #alias disable-pointer='xinput set-prop 13 "Device Enabled" 0'
 #alias enable-touchpad='xinput set-prop 12 "Device Enabled" 1'
 #alias disable-touchpad='xinput set-prop 12 "Device Enabled" 0'
-alias bat='for i in 0 1; do echo BAT$i ; upower -i "/org/freedesktop/UPower/devices/battery_BAT${i}" | grep --color=never "time to empty" ; done'
-alias bundle-cache-ls='aws s3 ls s3://grnds-test-coreos/'
+#alias bat='for i in 0 1; do echo BAT$i ; upower -i "/org/freedesktop/UPower/devices/battery_BAT${i}" | grep --color=never "time to empty" ; done'
+#alias bundle-cache-ls='aws s3 ls s3://grnds-test-coreos/'
 alias g='figlet -f doh G'
 
-bundle-cache-rm() {
-    aws s3 rm s3://grnds-test-coreos/${1}-bundle.tar.gz
-}
+#bundle-cache-rm() {
+#    aws s3 rm s3://grnds-test-coreos/${1}-bundle.tar.gz
+#}
 
 # docker
-alias dps='docker ps -a | less -S'
-alias docker-nodes="docker info | tail -n+6 | head -n-3 | grep -v '└' | tail -n+2"
-alias consul='docker run --rm --name consul gliderlabs/consul'
+#alias dps='docker ps -a | less -S'
+#alias docker-nodes="docker info | tail -n+6 | head -n-3 | grep -v '└' | tail -n+2"
+#alias consul='docker run --rm --name consul gliderlabs/consul'
 
 # grnds
-alias ae='gr-aws-environment'
-alias ae-dev='ae development'
-alias ae-ci='ae ci'
-alias ae-test='ae test'
-alias ae-i3='ae integration3'
-alias ae-uat='ae uat'
-alias te='tracker-environment'
-alias de='dev-environment'
-alias pe='papertrail-environment'
-alias he='honesty-environment'
+#alias ae='gr-aws-environment'
+#alias ae-dev='ae development'
+#alias ae-ci='ae ci'
+#alias ae-test='ae test'
+#alias ae-i3='ae integration3'
+#alias ae-uat='ae uat'
+#alias te='tracker-environment'
+#alias de='dev-environment'
+#alias pe='papertrail-environment'
+#alias he='honesty-environment'
 
 #function gnb()
 #{
@@ -119,9 +119,9 @@ alias he='honesty-environment'
     #git co -b `name-branch $1`
 #}
 
-grnds_dns() {
-  sudo sh -c 'echo search grandrounds.com >> /etc/resolv.conf'
-}
+#grnds_dns() {
+#  sudo sh -c 'echo search grandrounds.com >> /etc/resolv.conf'
+#}
 
 f() {
   awk "{print \$$1}"
@@ -135,12 +135,12 @@ for i in $(seq 10); do
   eval "alias f$i='f $i'"
 done
 
-sar() {
-  local pattern=$1
-  local from=$2
-  local to=$3
-  find . -type d -name .git -prune -o -type f -name "${pattern}" -print -exec sed -i -e"s/${from}/${to}/g" {} \;
-}
+#sar() {
+#  local pattern=$1
+#  local from=$2
+#  local to=$3
+#  find . -type d -name .git -prune -o -type f -name "${pattern}" -print -exec sed -i -e"s/${from}/${to}/g" {} \;
+#}
 
 google() {
   local query=$(python -c "import urllib, sys; print urllib.quote(sys.argv[1])" "$*")
@@ -154,17 +154,17 @@ bind '"\e[B": history-search-forward'
 bind '"\C-p": history-search-backward'
 bind '"\C-n": history-search-forward'
 
-remap_capslock() {
+#remap_capslock() {
   # remap caps lock to control
-  setxkbmap -option 'caps:ctrl_modifier'
+#  setxkbmap -option 'caps:ctrl_modifier'
 
   # make caps lock be esc when pressed alone
-  killall xcape &> /dev/null
-  ~/bin/xcape -e 'Caps_Lock=Escape'
+#  killall xcape &> /dev/null
+#  ~/bin/xcape -e 'Caps_Lock=Escape'
 
   # reload key repeat settings
-  touch ~/.config/lxsession/Lubuntu/desktop.conf
-}
+#  touch ~/.config/lxsession/Lubuntu/desktop.conf
+#}
 
 gpg_agent_start() {
   #export GPGKEY='F29B9AF3'
@@ -184,37 +184,37 @@ gpg_agent_start() {
   #fi
 }
 
-aws_env_short() {
-  case "${AWS_ENVIRONMENT}" in
-    'development')
-      echo 'dev'
-      ;;
-    'test')
-      echo 'test'
-      ;;
-    'ci')
-      echo 'ci'
-      ;;
-    'uat')
-      echo 'uat'
-      ;;
-    'integration')
-      echo 'i'
-      ;;
-    'integration1')
-      echo 'i1'
-      ;;
-    'integration2')
-      echo 'i2'
-      ;;
-    'integration3')
-      echo 'i3'
-      ;;
-    *)
-      return 1
-      ;;
-  esac
-}
+#aws_env_short() {
+#  case "${AWS_ENVIRONMENT}" in
+#    'development')
+#      echo 'dev'
+#      ;;
+#    'test')
+#      echo 'test'
+#      ;;
+#    'ci')
+#      echo 'ci'
+#      ;;
+#    'uat')
+#      echo 'uat'
+#      ;;
+#    'integration')
+#      echo 'i'
+#      ;;
+#    'integration1')
+#      echo 'i1'
+#      ;;
+#    'integration2')
+#      echo 'i2'
+#      ;;
+#    'integration3')
+#      echo 'i3'
+#      ;;
+#    *)
+#      return 1
+#      ;;
+#  esac
+#}
 
 prompt_function() {
   local        BLUE='\[\e[0;34m\]'
@@ -254,30 +254,30 @@ prompt_function() {
   #PS1="${LIGHT_GRAY}\u@\h: \w${git_color}$(__git_ps1)${LIGHT_GRAY}\$${RESET} "
   #PS1="${LIGHT_GRAY}[$(date +'%I:%M:%S%P %-m/%d')]\n${LIGHT_GRAY}\w${git_color}$(__git_ps1)${LIGHT_GRAY}\$${RESET} "
 
-  if [ -n "$SWARM_ENV" ]; then
-    if [[ "${SWARM_ENV}" == 'production' ]]; then
-      local swarm_env_short='prod'
-    else
-      local swarm_env_short="${SWARM_ENV}"
-    fi
-    local swarmy="${BLUE}(${swarm_env_short})"
-  else
-    local swarmy=''
-  fi
-
-  if aws_env_short > /dev/null ; then
-    local awsenv="${YELLOW}($(aws_env_short))"
-  else
-    local awsenv=''
-  fi
-
-  if ! uname -a | grep -q Darwin; then
-    if gr-my-vpn-ip > /dev/null ; then
-      local vpn_info="${PURPLE}(v)"
-    else
-      local vpn_info=''
-    fi
-  fi
+#  if [ -n "$SWARM_ENV" ]; then
+#    if [[ "${SWARM_ENV}" == 'production' ]]; then
+#      local swarm_env_short='prod'
+#    else
+#      local swarm_env_short="${SWARM_ENV}"
+#    fi
+#    local swarmy="${BLUE}(${swarm_env_short})"
+#  else
+#    local swarmy=''
+#  fi
+#
+#  if aws_env_short > /dev/null ; then
+#    local awsenv="${YELLOW}($(aws_env_short))"
+#  else
+#    local awsenv=''
+#  fi
+#
+#  if ! uname -a | grep -q Darwin; then
+#    if gr-my-vpn-ip > /dev/null ; then
+#      local vpn_info="${PURPLE}(v)"
+#    else
+#      local vpn_info=''
+#    fi
+#  fi
 
   #if [ -n "$TRACKER_TOKEN" ]; then
     #local tracker="${PURPLE}(t)"
@@ -285,12 +285,13 @@ prompt_function() {
     #local tracker=''
   #fi
 
-  PS1="${LIGHT_GRAY}\w${git_color}$(__git_ps1)${awsenv}${vpn_info}${swarmy}${LIGHT_GRAY}\$${RESET} "
+  #PS1="${LIGHT_GRAY}\w${git_color}$(__git_ps1)${awsenv}${vpn_info}${swarmy}${LIGHT_GRAY}\$${RESET} "
+  PS1="${LIGHT_GREEN}\w${git_color}$(__git_ps1)${WHITE}\$${RESET} "
 }
 
 PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ;} history -a ; prompt_function"
 
-tab_title() {
+tab() {
   if [[ ${PWD} == ${HOME} ]]; then
     TITLE='~'
   elif [[ ${PWD} == '/' ]]; then
@@ -312,12 +313,13 @@ tab_title() {
 }
 
 # auto-title
-if ! uname -a | grep -q Darwin; then
-  cd()    { __zsh_like_cd cd    "$@" && tab_title; }
-  pushd() { __zsh_like_cd pushd "$@" && tab_title; }
-  popd()  { __zsh_like_cd popd  "$@" && tab_title; }
-fi
-tab_title
+#if ! uname -a | grep -q Darwin; then
+#  cd()    { __zsh_like_cd cd    "$@" && tab_title; }
+#  pushd() { __zsh_like_cd pushd "$@" && tab_title; }
+#  popd()  { __zsh_like_cd popd  "$@" && tab_title; }
+#fi
+
+tab
 
 unswap_alt_super() {
   setxkbmap -layout us -option ''
@@ -332,34 +334,34 @@ fix_caps_lock() {
   setxkbmap -layout us -option ''
 }
 
-mount_smb() {
-  sudo PASSWD="$(gpg --batch -d ~/downloads/sfnas_xen-host_creds.txt.gpg 2> /dev/null | grep 'pwd: ' | awk '{print $2}')" \
-    mount -t cifs //sfnas.grandrounds.com/ISOs/ /mnt/sfnas/ -o rw,username=xen-host
-}
+#mount_smb() {
+#  sudo PASSWD="$(gpg --batch -d ~/downloads/sfnas_xen-host_creds.txt.gpg 2> /dev/null | grep 'pwd: ' | awk '{print $2}')" \
+#    mount -t cifs //sfnas.grandrounds.com/ISOs/ /mnt/sfnas/ -o rw,username=xen-host
+#}
 
-gr-gh-status() {
-  local repo="${1:?missing arg 1: repo}"
-  local sha="${2:?missing arg 2: sha}"
-  local state="${3:?missing arg 3: state (pending, success, error, or failure)}"
-  local desc="${4:?missing arg 4: desc}"
+#gr-gh-status() {
+#  local repo="${1:?missing arg 1: repo}"
+#  local sha="${2:?missing arg 2: sha}"
+#  local state="${3:?missing arg 3: state (pending, success, error, or failure)}"
+#  local desc="${4:?missing arg 4: desc}"
+#
+#  curl -s \
+#    "https://api.github.com/repos/ConsultingMD/${repo}/statuses/${sha}" \
+#    -H "Content-Type: application/json" \
+#    -H "authToken: TODO" \
+#    -XPOST \
+#    -d@- <<-HERE | jq .
+#{
+#  "state": "${state}",
+#  "description": "${desc}",
+#  "context": "developer-status"
+#}
+#HERE
+#}
 
-  curl -s \
-    "https://api.github.com/repos/ConsultingMD/${repo}/statuses/${sha}" \
-    -H "Content-Type: application/json" \
-    -H "authToken: TODO" \
-    -XPOST \
-    -d@- <<-HERE | jq .
-{
-  "state": "${state}",
-  "description": "${desc}",
-  "context": "developer-status"
-}
-HERE
-}
-
-docker-login() {
-  curl https://honesty.ci.grandrounds.com/docker_login.sh | bash
-}
+#docker-login() {
+#  curl https://honesty.ci.grandrounds.com/docker_login.sh | bash
+#}
 
 whereisfunc() {
   shopt -s extdebug ; declare -F "$1" ; shopt -s extdebug
