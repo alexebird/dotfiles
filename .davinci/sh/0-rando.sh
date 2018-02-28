29e8d25110c6c26dba2b4d5572c5c4c6e8702f99
Alex Bird <bird@stridehealth.com>
2018-02-27 16:44:28 -0800

    add bind for alt n,p

--- .davinci/sh/0-rando.sh
+++ .davinci/sh/0-rando.sh
@@ -13,3 +13,6 @@ bind '"\e[B": history-search-forward'
 # C-p/C-n
 bind '"\C-p": history-search-backward'
 bind '"\C-n": history-search-forward'
+# ALT-p/ALT-n (as workaround for weird docker cli behavior)
+bind '"\\M-p": history-search-backward'
+bind '"\\M-n": history-search-forward'
