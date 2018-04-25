27ca3c1969b54f728ef13a5d6b4d86635344160a
Alex Bird <bird@stridehealth.com>
2018-04-25 11:05:52 -0700

    fix DAVINCI_ENV_PATH

--- .infrared/conf.sh
+++ .infrared/conf.sh
@@ -1,7 +1,7 @@
 export DAVINCI_HOME="${HOME}/stride"
 export DAVINCI_CLONE="${HOME}/davinci"
 export DAVINCI_PATH="${HOME}/.davinci:${DAVINCI_HOME}/infrared"
-export DAVINCI_ENV_PATH="${HOME}/.davinci-env:${DAVINCI_HOME}/infrared/davinci"
+export DAVINCI_ENV_PATH="${HOME}/.davinci-env:${DAVINCI_HOME}/infrared/davinci/env"
 export DAVINCI_OPTS='prompt'
 
 export GPGP_PATH="${DAVINCI_HOME}/infrared"
