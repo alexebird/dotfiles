0b6d9ddec51833a1c407acc4d987e9427c12f6fd
Alexander Bird <bird@literal-bird-m1.local>
2021-11-05 16:34:02 -0700

    rm old files

--- .lein/profiles.clj
+++
@@ -1,8 +0,0 @@
-{
- :user  {:plugins [
-                   [venantius/ultra "0.5.1"]
-                   [lein-pprint "1.1.1"]
-                   ;[cider/cider-nrepl "0.7.0"]
-                   ]}
- ;:ultra {:color-scheme :solarized_dark}
- }
